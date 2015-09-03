c3b9eed3ec4ee7df5ed4301b941ba0045def26c9
Valentine Silvansky <v.silvansky@gmail.com>
2015-09-03 12:01:58 +0300

    Initial Commit

---
+++ photosort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  photosort
+//
+//  Created by Valentine on 03.09.15.
+//  Copyright © 2015 silvansky. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
